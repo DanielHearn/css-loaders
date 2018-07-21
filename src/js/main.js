f8de87c02ded25a827c4421c572cbe6969831f4b
Daniel Hearn <DanielHearn@users.noreply.github.com>
2018-07-21 22:19:20 +0100

    Style prev and next buttons

--- src/js/main.js
+++ src/js/main.js
@@ -82,7 +82,7 @@ const loaders = [
   },
   {
     name: '2: Heartbeat',
-    colour: '#1abc9c',
+    colour: '#25a78d',
     html:
 `<div class="pulseLoader">
     <div class="pulse"></div>
@@ -321,7 +321,9 @@ $dark: #34495e;
 
 const root = document.documentElement.style
 const loaderContainer = document.querySelector('#loader-container')
-const loaderContent = loaderContainer.querySelector('#loader-content')
+const loaderContent = loaderContainer.querySelector('#loader-content #loader')
+const loaderName = loaderContainer.querySelector('#loader-name h1')
+const loaderSource = loaderContainer.querySelector('#loader-source')
 const prevButton = loaderContainer.querySelector('#prevButton')
 const nextButton = loaderContainer.querySelector('#nextButton')
 const htmlElement = document.querySelector('#html-code')
@@ -354,17 +356,15 @@ scssButton.addEventListener('click', function() {
   
 function loadSlide (loader) {
   root.setProperty('--accent', loader.colour)
-  const slideName = document.createElement('h1')
-  slideName.textContent = loader.name
+  root.setProperty('--accent-dark', shadeColor(loader.colour,-0.2))
+  root.setProperty('--accent-darkish', shadeColor(loader.colour,-0.1))
+  loaderName.textContent = loader.name
   const animationWrapper = document.createElement('div')
   animationWrapper.id = 'animation-wrapper'
   animationWrapper.innerHTML = loader.html
   const sourceElement = document.createElement('a')
-  sourceElement.href = 'https://www.github.com/danielhearn/css-loaders'
-  sourceElement.textContent = 'View Source'
-  loaderContent.appendChild(slideName)
+  loaderSource.href = 'https://www.github.com/danielhearn/css-loaders'
   loaderContent.appendChild(animationWrapper)
-  loaderContent.appendChild(sourceElement)
 }
 
 function changeLoader (indexChange) {
@@ -454,5 +454,10 @@ function copyToClipboard(element) {
       document.getSelection().removeAllRanges();    // Unselect everything on the HTML document
       document.getSelection().addRange(selected);   // Restore the original selection
     }
-  };
+}
 
+// Function from https://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
+function shadeColor(color, percent) {   
+    var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+    return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
+}
