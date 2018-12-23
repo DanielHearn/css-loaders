459e44490bff8fccd09e30419b0b83ceb6063fed
DanielHearn <hearndaniel@live.co.uk>
2018-12-23 10:30:15 +0000

    Add bar loading anim

--- src/js/main.js
+++ src/js/main.js
@@ -467,8 +467,104 @@ $twoCircleColour2: #de4e40;
   54% { transform: scale(1); }
   100% { transform: scale(0); }
 }`
+  },
+  {
+    name: '7: Bar',
+    colour: '#4548c3',
+    html:
+`<div class="barLoader">
+  <div class="bar"></div>
+</div>`,
+    css:
+`.barLoader {
+  width: 100em;
+  max-width: 10em;
+}
+.barLoader .bar {
+  width: 100%;
+  opacity: 0;
+  height: 1.5em;
+  background: #fefefe;
+  animation: baranim 2.5s infinite cubic-bezier(0.61, 0.33, 0.39, 0.79);
+  transform-origin: 0% 50%;
+}
+
+@keyframes baranim {
+  0% {
+    transform: scaleX(0);
+    opacity: 0;
+  }
+  5% {
+    transform: scaleX(0);
+    opacity: 0;
+  }
+  10% {
+    opacity: 1;
+  }
+  90% {
+    opacity: 1;
+  }
+  95% {
+    transform: scaleX(1);
+  }
+  100% {
+    transform: scaleX(1);
+    opacity: 0;
+  }
+}`,
+    scss:
+`$barTiming: 2.5s;
+$barHeight: 1.5em;
+$barMaxWidth: 10em;
+$barColor: #fefefe;
+
+.barLoader {
+  width: 100em;
+  max-width: $barMaxWidth;
+  .bar {
+    width: 100%;
+    opacity: 0;
+    height: $barHeight;
+    background: $barColor;
+    animation: baranim $barTiming infinite cubic-bezier(0.61, 0.33, 0.39, 0.79);
+    transform-origin: 0% 50%;
+  }
+}
+
+@keyframes baranim {
+  0% {
+    transform: scaleX(0);
+    opacity: 0;
+  }
+  5% {
+    transform: scaleX(0);
+    opacity: 0;
+  }
+  10% {
+    opacity: 1;
+  }
+  90% {
+    opacity: 1;
+  }
+  95% {
+    transform: scaleX(1);
+  }
+  100% {
+    transform: scaleX(1);
+    opacity: 0;
+  }
+}`
   }
 ]
+
+// Formatting for css loader objects
+/* {
+  name: 'NUM: Name',
+  colour: 'HEX COLOUR',
+  html: `HTML`,
+  css: `CSS`,
+  scss: `SCSS`
+} */
 
 const root = document.documentElement.style
 const loaderContainer = document.querySelector('#loader-container')
