e047a16c0fd231f1a73784f2268e99a218dce736
Daniel Hearn <DanielHearn@users.noreply.github.com>
2020-06-25 18:33:44 +0100

    Fix useContainerDimensions hook warning

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -8,14 +8,14 @@ export function useTitle(title: string) {
 
 // Adapted from https://stackoverflow.com/questions/43817118/how-to-get-the-width-of-a-react-element
 export const useContainerDimensions = ref => {
-  const getDimensions = () => ({
-    width: ref.current.offsetWidth,
-    height: ref.current.offsetHeight
-  })
-
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 })
 
   useEffect(() => {
+    const getDimensions = () => ({
+      width: ref.current.offsetWidth,
+      height: ref.current.offsetHeight
+    })
+
     const handleResize = () => {
       setDimensions(getDimensions())
     }
@@ -29,7 +29,7 @@ export const useContainerDimensions = ref => {
     return () => {
       window.removeEventListener("resize", handleResize)
     }
-  }, [ref])
+  }, [ref] )
 
   return dimensions;
 };
