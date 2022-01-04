fc5063d451260668dd7f22ab10da51d10f6bad00
Daniel Hearn <DanielHearn@users.noreply.github.com>
2022-01-04 18:51:22 +0000

    Fix loader view formatting

--- src/views/loaders/index.js
+++ src/views/loaders/index.js
@@ -35,102 +35,102 @@ export default function Loaders({screen = ScreenTypes.LARGE_SCREEN} : Props) : N
   useTitle(`${capitaliseWords(unslugify(loaderName))} - ${titleRoot}`)
   const smallScreen = screen === ScreenTypes.SMALL_SCREEN
 
-    const storedLoader = loaders.filter((loader: LoaderType) : boolean => 
-      loaderName === slugify(loader.name)
-    )[0]
+  const storedLoader = loaders.filter((loader: LoaderType) : boolean => 
+    loaderName === slugify(loader.name)
+  )[0]
 
-    if (storedLoader) {
-      let codeTabs = [];
-      const languages = Object.keys(storedLoader.code)
-      for (const language of languages) {
-        codeTabs.push(
-          {
-            id: `${slugify(storedLoader.name)}_${language}`,
-            name: capitaliseAll(unslugify(language)),
-            content: <CodeBlock language={language} code={storedLoader.code[language]}/>
-          }
-        )
-      }
+  if (storedLoader) {
+    let codeTabs = [];
+    const languages = Object.keys(storedLoader.code)
+    for (const language of languages) {
+      codeTabs.push(
+        {
+          id: `${slugify(storedLoader.name)}_${language}`,
+          name: capitaliseAll(unslugify(language)),
+          content: <CodeBlock language={language} code={storedLoader.code[language]}/>
+        }
+      )
+    }
 
-      if (smallScreen) {
-        const mobileNavTabs = [
-          {
-            id: `${slugify(storedLoader.name)}_animation`,
-            name: 'Animation',
-            icon: <TiMediaPlay/>,
-            content: (
-              <div className="row" style={{position: 'relative', height: '100%'}}>
-                <h1 className="heading text-center text-white" style={{position: 'absolute', top: '20%', width: '100%'}}>{storedLoader.name}</h1>
-                <div style={{display: 'flex', flex: 1, justifyContent: 'center', alignItems: 'center', background: storedLoader.color}}>
-                  <Loader html={storedLoader.code.html} css={storedLoader.code.css}/>
-                </div>
-              </div>
-            )
-          },
-          {
-            id: `${slugify(storedLoader.name)}_code`,
-            name: 'Code',
-            icon: <TiCode/>,
-            content: (
-              <Tabs tabs={codeTabs}/>
-            )
-          },
-          {
-            id: `${slugify(storedLoader.name)}_library`,
-            name: 'Library',
-            icon: <TiThSmall/>,
-            content: (
-              <div className="row" style={{height: '100%'}}>
-                <div className="content_container">
-                    <ScrollBox y={true}>
-                      <div className="content">
-                        <div style={{marginBottom: '4em'}}>
-                          <LoaderSelection initialActiveLoader={loaderName}/>
-                        </div>
-                      </div>
-                    </ScrollBox>
-                </div>
-              </div>
-            )
-          }
-        ]
-
-        return (
-          <div className="row" style={{height: '90%'}}>
-            <MobileNav tabs={mobileNavTabs} defaultTab={smallScreen && !loaderMatch ? mobileNavTabs[2] :null}/>
-          </div>
-        )
-      } else {
-        return (
-          <div className="row" style={{height: '100%'}}>
-            <div className="column" style={{width: '50%', height: '100%'}}>
-              <div className="row" style={{position: 'relative', height: '50%'}}>
-                <h1 className="heading text-center text-white" style={{position: 'absolute', top: '20%', width: '100%'}}>{storedLoader.name}</h1>
-                <div style={{display: 'flex', flex: 1, justifyContent: 'center', alignItems: 'center', background: storedLoader.color}}>
-                  <Loader html={storedLoader.code.html} css={storedLoader.code.css}/>
-                </div>
-              </div>
-              <div className="row" style={{height: '50%'}}>
-                <div className="content_container">
-                    <ScrollBox y={true}>
-                      <div className="content">
-                        <div style={{marginBottom: '4em'}}>
-                          <LoaderSelection/>
-                        </div>
-                      </div>
-                    </ScrollBox>
-                </div>
+    if (smallScreen) {
+      const mobileNavTabs = [
+        {
+          id: `${slugify(storedLoader.name)}_animation`,
+          name: 'Animation',
+          icon: <TiMediaPlay/>,
+          content: (
+            <div className="row" style={{position: 'relative', height: '100%'}}>
+              <h1 className="heading text-center text-white" style={{position: 'absolute', top: '20%', width: '100%'}}>{storedLoader.name}</h1>
+              <div style={{display: 'flex', flex: 1, justifyContent: 'center', alignItems: 'center', background: storedLoader.color}}>
+                <Loader html={storedLoader.code.html} css={storedLoader.code.css}/>
               </div>
             </div>
-            <div className="column" style={{width: '50%'}}>
-              <Tabs tabs={codeTabs}/>
+          )
+        },
+        {
+          id: `${slugify(storedLoader.name)}_code`,
+          name: 'Code',
+          icon: <TiCode/>,
+          content: (
+            <Tabs tabs={codeTabs}/>
+          )
+        },
+        {
+          id: `${slugify(storedLoader.name)}_library`,
+          name: 'Library',
+          icon: <TiThSmall/>,
+          content: (
+            <div className="row" style={{height: '100%'}}>
+              <div className="content_container">
+                  <ScrollBox y={true}>
+                    <div className="content">
+                      <div style={{marginBottom: '4em'}}>
+                        <LoaderSelection initialActiveLoader={loaderName}/>
+                      </div>
+                    </div>
+                  </ScrollBox>
+              </div>
             </div>
-          </div>
-        )
-      }
+          )
+        }
+      ]
+
+      return (
+        <div className="row" style={{height: '90%'}}>
+          <MobileNav tabs={mobileNavTabs} defaultTab={smallScreen && !loaderMatch ? mobileNavTabs[2] :null}/>
+        </div>
+      )
     } else {
       return (
-        <Redirect url="/"/>
-      );
+        <div className="row" style={{height: '100%'}}>
+          <div className="column" style={{width: '50%', height: '100%'}}>
+            <div className="row" style={{position: 'relative', height: '50%'}}>
+              <h1 className="heading text-center text-white" style={{position: 'absolute', top: '20%', width: '100%'}}>{storedLoader.name}</h1>
+              <div style={{display: 'flex', flex: 1, justifyContent: 'center', alignItems: 'center', background: storedLoader.color}}>
+                <Loader html={storedLoader.code.html} css={storedLoader.code.css}/>
+              </div>
+            </div>
+            <div className="row" style={{height: '50%'}}>
+              <div className="content_container">
+                  <ScrollBox y={true}>
+                    <div className="content">
+                      <div style={{marginBottom: '4em'}}>
+                        <LoaderSelection/>
+                      </div>
+                    </div>
+                  </ScrollBox>
+              </div>
+            </div>
+          </div>
+          <div className="column" style={{width: '50%'}}>
+            <Tabs tabs={codeTabs}/>
+          </div>
+        </div>
+      )
     }
+  } else {
+    return (
+      <Redirect url="/"/>
+    );
+  }
 }
