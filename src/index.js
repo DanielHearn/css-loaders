a4c275a05a4997cca7bf91419288d53bc70914da
Daniel Hearn <DanielHearn@users.noreply.github.com>
2022-08-22 17:21:20 +0100

    Fix service worker error

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
 import App from './components/app/';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
